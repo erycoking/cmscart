c68e7c36f5a90f569bd23d350931315d3ef43703
erick <erick@upskillable.com>
2020-04-11 21:32:48 +0300

    added and configured more dependencies

--- app.js
+++ app.js
@@ -5,6 +5,11 @@
 
 const express = require('express');
 const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const connectFlash = require('connect-flash');
+const expressMessages = require('express-messages');
 
 // setting up database
 const mongoose = require('mongoose');
@@ -13,6 +18,7 @@ mongoose.connect(config.database, {useNewUrlParser: true, useUnifiedTopology: tr
 
 // Init APP
 const app = express();
+app.use(logger('dev'))
 
 // View engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,9 +27,37 @@ app.set('view engine', 'ejs');
 // Set Up public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.render('index');
+// body parser middleware
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// express session middleware
+app.use(session({
+  secret: 'awesome coders',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+
+// express message middleware
+app.use(connectFlash());
+app.use((req, res, next) => {
+  res.locals.messages = expressMessages(req, res);
+  next();
 });
+
+
+// configure all routes
+// admin routes
+const adminPages = require('./routes/admin_pages.js');
+app.use('/admin', adminPages);
+
+// user routes
+const userPages = require('./routes/pages.js');
+app.use('/', userPages);
 
 const port = process.env.PORT || 3000;
 const env = process.env.NODE_ENV || 'production';
